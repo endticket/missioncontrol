238093359833811a96b7f6f42d02527593319d8a
deathowl <bagoly@endticket.com>
2015-07-15 01:44:02 +0200

    override listen port

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -4,13 +4,23 @@ use std::process::Command;
 use std::net::{TcpListener,TcpStream};
 use std::env;
 use std::thread;
+use std::mem;
 
+
+fn string_to_static_str(s: String) -> &'static str {
+    unsafe {
+        let ret = mem::transmute(&s as &str);
+        mem::forget(s);
+        ret
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
     let mut opts = Options::new();
     opts.optflag("d", "daemon", "convert this into a daemon");
+    opts.optopt("l", "listen", "Listen on", "LISTEN");
 
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
@@ -20,15 +30,19 @@ fn main() {
     // Create a daemon? if necessary
     if matches.opt_present("d") {
         let child = Command::new(program).spawn().unwrap();
-        //println!("Created child: {}", child.id());
-        //child.forget();
         return;
     }
+    let listen_on = match matches.opt_str("l") {
+        Some(x) => string_to_static_str(x),
+        None => "127.0.0.1:37565"
+    };
 
-    let listener = TcpListener::bind("127.0.0.1:37565").unwrap();
+    println!("{:?}", listen_on);
+
+    let listener = TcpListener::bind(listen_on).unwrap();
 
     fn handle_client(stream: TcpStream) {
-
+        //TODO
     }
 
     // accept connections and process them, spawning a new thread for each one
